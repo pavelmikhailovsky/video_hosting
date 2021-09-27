cf44d4ad2ed391a741a1e8c8ab0e6147b311aae2
pavelmikhailovsky <dsvknsddfasfawe@gmail.com>
2021-09-27 20:23:15 +0300

    add new endpoint

--- src/users/api.py
+++ src/users/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from fastapi.security import OAuth2PasswordRequestForm
+from  fastapi.security import HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
 
 from . import service, auth, schemas
@@ -9,15 +9,22 @@ users_app = APIRouter(prefix='/users', tags=['users'])
 
 
 @users_app.post('/token')
-async def login(db: Session = Depends(get_db), data: OAuth2PasswordRequestForm = Depends()):
+async def login(db: Session = Depends(get_db), data: schemas.UserTokenLogin = Depends()):
     return service.access_token(db, data)
 
 
-@users_app.get('/me')
-async def users_me(db: Session = Depends(get_db), token: str = Depends(auth.oauth2_scheme)):
+@users_app.get('/me', response_model=schemas.User)
+async def users_me(db: Session = Depends(get_db), token: HTTPAuthorizationCredentials = Depends(auth.oauth2_scheme)):
     return service.get_current_user(db, token)
 
 
-@users_app.post('/create')
-async def create_user(data: schemas.UserInDB, db: Session = Depends(get_db)):
-    return service.create_user(data, db)
+@users_app.post('/create', response_model=schemas.UserInDB)
+async def create_user(data: schemas.UserInDB = Depends(), db: Session = Depends(get_db)):
+    return service.create_user(db, data)
+
+
+@users_app.put('/update')
+async def update_user(db: Session = Depends(get_db),
+                      data: schemas.UserUpdate = Depends(),
+                      token: HTTPAuthorizationCredentials = Depends(auth.oauth2_scheme)):
+    return service.UpdateUser(db, data, token).returning_result()
