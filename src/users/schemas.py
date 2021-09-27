80161ff64026f4c8b0c34baff7645fb67d111dd6
pavelmikhailovsky <dsvknsddfasfawe@gmail.com>
2021-09-27 20:25:01 +0300

    is changed scheme authorization

--- src/users/schemas.py
+++ src/users/schemas.py
@@ -10,11 +10,25 @@ class User(BaseModel):
     id: int
     username: str
     create_at: datetime
-    video: Video
+    video: t.List[Video] = []
     is_active: bool
     is_staff: bool
+
+    class Config:
+        orm_mode = True
 
 
 class UserInDB(BaseModel):
     username: str
     password: str
+
+
+class UserTokenLogin(BaseModel):
+    username: str
+    password: str
+
+
+class UserUpdate(BaseModel):
+    new_username: t.Optional[str]
+    current_password: t.Optional[str]
+    new_password: t.Optional[str]
