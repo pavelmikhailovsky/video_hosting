6cf7a02f8cb1921eed0f7844892db4276e765eaf
pavelmikhailovsky <dsvknsddfasfawe@gmail.com>
2021-10-01 20:31:45 +0300

    upgrade test database

--- core/tests/test_db.py
+++ core/tests/test_db.py
@@ -1,16 +1,26 @@
 from sqlalchemy.engine import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy_utils import create_database, database_exists, drop_database
 from starlette.requests import Request
-from starlette.responses import Response
 
 from core.db import Base
 
+url_database = 'postgresql://postgres:pavel@localhost/test_video_hosting'
 
-engine = create_engine('postgresql://postgres:pavel@localhost/test_video_hosting')
+if database_exists(url_database):
+    drop_database(url_database)
 
+create_database(url_database)
+
+engine = create_engine(url_database)
 TestSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base.metadata.create_all(engine)
+# импорты обязательны только после инициализации Base
+from src.users.models import User
+from src.video.models import Video
+
+Base.metadata.create_all(bind=engine)
 
 
 def override_get_db(request: Request):
